89fe4475b27f8c3882f1c678c733fc393ce337d7
unknown <kamila.wilczynska92@gmail.com>
2022-09-10 12:55:10 +0100

    find the correct article and add class active

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
   console.log('"clickedElement": ' + clickedElement);
 
@@ -33,16 +33,16 @@ for (let activeArticle of activeArticles) {
   activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
   
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
   console.log('TargetArticle: ' + articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
   console.log(articleSelector);
 }
